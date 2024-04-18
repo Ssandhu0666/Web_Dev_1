a60a985c98b186a8bbddc3548a42a9e74b0fd2cf
Ssandhu0666 <167256011+Ssandhu0666@users.noreply.github.com>
2024-04-18 09:51:17 -0600

    Add files via upload

---
+++ scripts/booking.js
@@ -0,0 +1,99 @@
+/********* create variables *********/
+// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
+// Do any of these variables need to be initialized when the page is loaded? 
+// When do they need to be reset or updated?
+
+var the_cost_per_day = 35;
+var the_number_of_days_selected = 0;
+var total_cost = 0;
+
+var mon = document.getElementById("monday");
+var tues = document.getElementById("tuesday");
+var wed = document.getElementById("wednesday");
+var thurs = document.getElementById("thursday");
+var fri = document.getElementById("friday");
+
+var full = document.getElementById("full");
+var half = document.getElementById("half");
+var clear_days = document.getElementById("clear-button");
+
+var calculated_cost = document.getElementById("calculated-cost");
+
+
+/********* colour change days of week *********/
+// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
+// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
+
+function add_color() {
+    if (this.classList.contains("blue-hover")) {
+      this.classList.add("clicked");
+      the_number_of_days_selected += 1;
+    }
+  
+    total_cost = the_cost_per_day * the_number_of_days_selected;
+    calculated_cost.innerHTML = total_cost;
+  }
+  
+  mon.addEventListener("click", add_color);
+  tues.addEventListener("click", add_color);
+  wed.addEventListener("click", add_color);
+  thurs.addEventListener("click", add_color);
+  fri.addEventListener("click", add_color);
+  
+  full.addEventListener("click", add_color);
+  half.addEventListener("click", add_color);
+  
+
+
+/********* clear days *********/
+// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+
+function remove_color() {
+  // document.writeln("mon")
+  //mon.classList.remove('clicked');
+  mon.classList.remove("clicked");
+  tues.classList.remove("clicked");
+  wed.classList.remove("clicked");
+  thurs.classList.remove("clicked");
+  fri.classList.remove("clicked");
+
+  total_cost = 0;
+  calculated_cost.innerHTML = total_cost;
+}
+
+clear_days.addEventListener("click", remove_color);
+
+
+
+/********* change rate *********/
+// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+
+
+function half_day() {
+    the_cost_per_day = 20;
+    half.classList.add("clicked");
+    full.classList.remove("clicked");
+    total_cost = the_cost_per_day * the_number_of_days_selected;
+    calculated_cost.innerHTML = total_cost;
+  }
+  
+  half.addEventListener("click", half_day);
+  
+
+// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+
+
+function full_day() {
+    the_cost_per_day = 35;
+    full.classList.add("clicked");
+    half.classList.remove("clicked");
+    total_cost = the_cost_per_day * the_number_of_days_selected;
+    calculated_cost.innerHTML = total_cost;
+  }
+  
+  full.addEventListener("click", full_day);
+
+
+/********* calculate *********/
+// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
+
